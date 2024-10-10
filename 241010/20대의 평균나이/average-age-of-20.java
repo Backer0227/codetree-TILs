a841d83293d4b18f7f344070fc364f758c628aef
Backer0227 <backjc10@gmail.com>
2024-10-10 13:41:17 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241010/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0, count = 0;
+        while(true)
+        {
+            int a = sc.nextInt();
+            if (a > 0 && a < 30)
+            {
+                sum += a;
+                count ++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        double avg = ((double)sum)/count;
+        System.out.printf("%.2f",avg);
+    }
+}
