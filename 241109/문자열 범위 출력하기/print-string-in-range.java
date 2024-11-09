817290b0da9da174d04a062f952481d5ed8d7487
Backer0227 <backjc10@gmail.com>
2024-11-09 18:13:00 +0900

    Title: 문자열 범위 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241109/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.nextLine();
+
+        for(int i=2;i<10;i++)
+        {
+            System.out.print(A.charAt(i));
+        }
+    }
+}
