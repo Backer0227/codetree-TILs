333a86fa81db1ac73153a96d8c08a9276637a10f
Backer0227 <backjc10@gmail.com>
2024-11-21 19:46:51 +0900

    Title: 함수를 이용한 369 게임, Time: 233ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 241121/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean threes(int n)
+    {
+        String nString = Integer.toString(n);
+        int[] arr = new int[nString.length()];
+        for(int i=0;i<nString.length();i++)
+        {
+            arr[i] = nString.charAt(i);
+            if(arr[i]%3 == 0 || n%3 == 0)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+    public static int sum(int a, int b)
+    {
+        int count = 0;
+        for(int i=a;i<=b;i++)
+        {
+            if(threes(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(sum(a,b));
+
+    }
+}
