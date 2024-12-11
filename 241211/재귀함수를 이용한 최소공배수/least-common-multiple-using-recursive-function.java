d61c9dd57cffaae8fea776b35e6bb50d24223737
Backer0227 <backjc10@gmail.com>
2024-12-11 17:13:15 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241211/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int recursion(int[] arr, int n)
+    {
+        if(n == 0)
+        {
+            return arr[0];
+        }
+        int A = recursion(arr,n-1);
+        if(A%arr[n] == 0)
+        {
+            return A;
+        }
+        else
+        {
+            return A*arr[n];
+        }
+
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print(recursion(arr,n-1));
+    }
+}
